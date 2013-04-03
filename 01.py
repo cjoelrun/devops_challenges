2133ac7f21f18abb4c00ac35e9530f5ef43c898d
Cameron Lopez <cameronjlopez@gmail.com>
2013-04-03 13:56:37 -0500

    clean imports, clean args, fix indention

--- 01.py
+++ 01.py
@@ -1,10 +1,8 @@
 import novaclient.client
-import novaclient.exceptions
 import os
 import argparse
 import sys
 import time
-from pprint import pprint
 
 """
 A script that builds three 512 MB Servers that follow a similar naming
@@ -14,28 +12,28 @@ convention and returns the IP and login credentials for each server.
 # Acquire arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--user', action="store", dest="user",
-                    required=False, default=os.getenv("OS_USERNAME"),
+                    default=os.getenv("OS_USERNAME"),
                     help="Openstack username")
 parser.add_argument('--password', action="store", dest="password",
-                    required=False, default=os.getenv("OS_PASSWORD"),
+                    default=os.getenv("OS_PASSWORD"),
                     help="Openstack password")
 parser.add_argument('--tenant_name', action="store", dest="tenant_name",
-                    required=False, default=os.getenv("OS_TENANT_NAME"),
+                    default=os.getenv("OS_TENANT_NAME"),
                     help="Openstack Tenant Name")
 parser.add_argument('--url', action="store", dest="url", required=False,
                     default=os.getenv("OS_AUTH_URL"),
                     help="URL of OpenStack endpoint")
 parser.add_argument('--server_name', action="store", dest="server_name",
-                    required=False, default="devops",
-                    help="Name for servers to create")
+                    default="devops", help="Name for servers to create")
 parser.add_argument('--num_servers', action="store", dest="num_servers",
-                    required=False, default=3,
-                    help="Number of servers to create")
+                    default=3, help="Number of servers to create")
 args = parser.parse_args()
+
 # Make sure all arguments have been defined
-if not all(vars(args)[k] for k in vars(args).keys()):
+undefined = list(k for k in vars(args).keys() if not vars(args)[k])
+if undefined:
     parser.print_usage()
-    print "Undefined: " + ", ".join(k for k in vars(args).keys() if not vars(args)[k])
+    print "Undefined: " + ", ".join(undefined)
     sys.exit(1)
 
 # Acquire a nova client
@@ -51,8 +49,8 @@ image = next(image for image in nova.images.list() if image.name == "precise")
 servers = []
 for n in range(args.num_servers):
     servers.append(nova.servers.create(name="%s-%s" % (args.server_name, n+1), 
-                                 flavor=flavor,
-                                 image=image))
+                                       flavor=flavor,
+                                       image=image))
 
 # Wait for networking to happen to print credentials
 for server in servers:
@@ -67,5 +65,4 @@ for server in servers:
             break
     else:
         print "server error: %s" % server
-
-    
+        
